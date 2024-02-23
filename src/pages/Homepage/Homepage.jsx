85aa886ef8ccf56fa13a9a749009e0fb52af64b7
garikdallari <garikdallari@gmail.com>
2024-02-23 09:22:34 +0200

    add sass structure, fonts, images, header

---
+++ src/pages/Homepage/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from "../../components/Header";
+
+const Homepage = () => {
+  return (
+    <div className='homepage'>
+      <Header />
+    </div>
+  )
+}
+
+export default Homepage

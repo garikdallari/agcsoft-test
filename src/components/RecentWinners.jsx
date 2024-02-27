c6e0368f59e3314741bb6c3229e8cfd2c81d3408
garikdallari <garikdallari@gmail.com>
2024-02-27 15:04:15 +0200

    add desktop styles

--- src/components/RecentWinners.jsx
+++ src/components/RecentWinners.jsx
@@ -5,84 +5,72 @@ import recentWinImg from "../images/recentWinners/Rectangle12.png";
 const RecentWinners = () => {
   return (
     <div className="recentWinners">
-      <div className="recentWinners__topBg">
-        <div className="recentWinners__topBg--topBg1"></div>
-        <div className="recentWinners__topBg--topBg2"></div>
-      </div>
-      <div className="recentWinners__content">
-        <div className="recentWinners__logo">
-          <div className="recentWinners__logo--image">
-            <img src={recentWinnersLogo} alt="recentwinners" />
-          </div>
-          <div className="recentWinners__logo--text">
-            <p className="recentWinners__logo--text">RECENT</p>
-            <p className="recentWinners__logo--text recentWinners__logo--text--highlighted">
-              WINNERS
-            </p>
-          </div>
+      <div className="recentWinners__wrapper">
+        <div className="recentWinners__topBg">
+          <div className="recentWinners__topBg--topBg1"></div>
+          <div className="recentWinners__topBg--topBg2"></div>
         </div>
-        <div className="recentWinners__elements">
-          <div className="recentWinners__element">
-            <div className="recentWinners__element--text">
-              <p className="">Robert - €18.75</p>
-              <p className="">
-                in{" "}
-                <span className="recentWinners__element--text--highlighted">
-                  Sun of Egypt
-                </span>
-              </p>
+        <div className="recentWinners__content">
+          <div className="recentWinners__logo">
+            <div className="recentWinners__logo--image">
+              <img src={recentWinnersLogo} alt="recentwinners" />
             </div>
-            <div className="recentWinners__element--image">
-              <img src={recentWinImg} alt="recentwin" />
+            <div className="recentWinners__logo--text">
+              <p className="recentWinners__logo--text">RECENT</p>
+              <p className="recentWinners__logo--text recentWinners__logo--text--highlighted">
+                WINNERS
+              </p>
             </div>
           </div>
-          <div className="recentWinners__element">
-            <div className="recentWinners__element--text">
-              <p className="">Robert - €18.75</p>
-              <p className="">
-                in{" "}
-                <span className="recentWinners__element--text--highlighted">
-                  Crystal Fruits
-                </span>
-              </p>
+          <div className="recentWinners__elements">
+            <div className="recentWinners__element">
+              <div className="recentWinners__element--text">
+                <p className="">Robert - €18.75</p>
+                <p className="">
+                  in{" "}
+                  <span className="recentWinners__element--text--highlighted">
+                    Sun of Egypt
+                  </span>
+                </p>
+              </div>
+              <div className="recentWinners__element--image">
+                <img src={recentWinImg} alt="recentwin" />
+              </div>
             </div>
-            <div className="recentWinners__element--image">
-              <img src={recentWinImg} alt="recentwin" />
+            <div className="recentWinners__element">
+              <div className="recentWinners__element--text">
+                <p className="">Robert - €18.75</p>
+                <p className="">
+                  in{" "}
+                  <span className="recentWinners__element--text--highlighted">
+                    Crystal Fruits
+                  </span>
+                </p>
+              </div>
+              <div className="recentWinners__element--image">
+                <img src={recentWinImg} alt="recentwin" />
+              </div>
             </div>
-          </div>
-          <div className="recentWinners__element">
-            <div className="recentWinners__element--text">
-              <p className="">Robert - €18.75</p>
-              <p className="">
-                in{" "}
-                <span className="recentWinners__element--text--highlighted">
-                  Crystal Fruits
-                </span>
-              </p>
-            </div>
-            <div className="recentWinners__element--image">
-              <img src={recentWinImg} alt="recentwin" />
-            </div>
-          </div>
-          <div className="recentWinners__element">
-            <div className="recentWinners__element--text">
-              <p className="">Robert - €18.75</p>
-              <p className="">
-                in{" "}
-                <span className="recentWinners__element--text--highlighted">
-                  Crystal Fruits
-                </span>
-              </p>
-            </div>
-            <div className="recentWinners__element--image">
-              <img src={recentWinImg} alt="recentwin" />
+            <div className="recentWinners__element">
+              <div className="recentWinners__element--text">
+                <p className="">Robert - €18.75</p>
+                <p className="">
+                  in{" "}
+                  <span className="recentWinners__element--text--highlighted">
+                    Crystal Fruits
+                  </span>
+                </p>
+              </div>
+              <div className="recentWinners__element--image">
+                <img src={recentWinImg} alt="recentwin" />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="recentWinners__bottomBg">
-        <div className="recentWinners__bottomBg--bottomBg1"></div>
-        <div className="recentWinners__bottomBg--bottomBg2"></div>
+        <div className="recentWinners__bottomBg">
+          <div className="recentWinners__bottomBg--bottomBg1"></div>
+          <div className="recentWinners__bottomBg--bottomBg2"></div>
+        </div>
       </div>
     </div>
   );
